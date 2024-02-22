ecd97bb96c21e78d50fe7b21e5079076727880c3
Shinten01 <clalabel.s.k@gmail.com>
2024-02-23 00:37:01 +0900

    I added plotting data

--- data_loading.py
+++ data_loading.py
@@ -1,6 +1,7 @@
+import pandas as pd
+import numpy as np
 
-
-filename = './data/boeing_practice.csv'
+filename = '/Users/shinten/Desktop/space_econometrics/figures/data/boeing_practice.csv'
 
 mydata = pd.read_csv(filename)
 print(mydata)
