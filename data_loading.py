e7540bfdedb8d69322cf0398014f929055698b56
Shinten01 <clalabel.s.k@gmail.com>
2024-03-13 02:15:28 +0900

    new dta

--- data_loading.py
+++ data_loading.py
@@ -1,13 +1,15 @@
+import pandas_datareader as pdr
 import pandas as pd
 import numpy as np
 import json
 
 # df = pd.read_excel('path/to/sheet.xlsx', 'sheet_name')
-company_keys = './company_keys.json'
-filename = './financial_data/planet_compiled.xlsx'
+#company_keys = './company_keys.json'
+#filename = './financial_data/planet_compiled.xlsx'
+filename = './financial_data/iQPS.xlsx'
 sheet_name = 'income' # 'income' or 'consolidated'
 
-keys = json.load(open(company_keys))
-print(keys["planet"]["sheet"])
-mydata = pd.read_excel(f'./financial_data/{keys["planet"]["file"]}', keys["planet"]["sheet"])
-print(mydata)
+#keys = json.load(open(company_keys))
+#print(keys["planet"]["sheet"])
+mydata = pd.read_excel(filename,sheet_name)
+print(mydata["Current Assets"])
